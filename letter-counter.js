01c07e425d6085c1a715bfd33416833f918d4d00
Dylan Vernon <dylanvernon22@gmail.com>
2021-09-26 12:36:17 -0700

    Transferred all files from local repo to GitHub repo

---
+++ letter-counter.js
@@ -0,0 +1,30 @@
+import { DataProcessor } from './components/data-processor.js';
+let letterCounterStructure = {
+    id: 'letter-counter',
+    title: 'Letter Counter',
+    buttons: {
+        'Add Text': function addText() {
+            let inputContainer= letterCounterProperties.project.container.inputContainer.element;
+            let newText = prompt("Please type in some text.");
+            inputContainer.textContent += newText;
+        },
+        'Count Letters': function countLetters() {
+            let inputContainer = letterCounterProperties.project.container.inputContainer.element;
+            let outputContainer = letterCounterProperties.project.container.outputContainer.element;
+            let content = inputContainer.textContent;
+            content = content.replace(/[^a-zA-Z]/g, '');
+            let count = content.length;
+            outputContainer.textContent = `Letter Count: ${count} letter(s)`;
+        },
+        'Remove Text': function removeText() {
+            let inputContainer = letterCounterProperties.project.container.inputContainer.element;
+            inputContainer.textContent = '';
+            letterCounterStructure.buttons['Count Letters']();
+        }
+    }
+}
+
+let letterCounterProperties = {
+        project: new DataProcessor(letterCounterStructure)
+}
+document.getElementById('text-processors').appendChild(letterCounterProperties.project.element);
